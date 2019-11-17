99b2c3197a48e4ed487c3513fa359cfdf4227ba0
Chris Hughes <chris@speedycomputing.com>
2019-11-16 16:17:12 -0800

    misspelling

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -14,16 +14,16 @@ module.exports = {
   create(request, response) {
     Task.create(request.body)
       .then(task => response.json(task))
-      .catch(err => res.json(err));
+      .catch(err => response.json(err));
   },
   update(request, response) {
     Task.findByIdAndUpdate(request.params.id, request.body, { new: true })
       .then(task => response.json(task))
-      .catch(err => res.json(err));
+      .catch(err => response.json(err));
   },
   destroy(request, response) {
     Task.findByIdAndRemove(request.params.id)
       .then(result => response.json(result))
-      .catch(err => res.json(err));
+      .catch(err => response.json(err));
   },
 };
